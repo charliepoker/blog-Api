38640e3ba24a75624fa80bf5892e68732e3ee270
charliepoker <iheanachocharlie@gmail.com>
2022-07-09 13:03:43 +0100

    added sorting support.

--- src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
@@ -31,9 +31,10 @@ public class PostController {
     @GetMapping
     public PostResponse getAllPosts(
             @RequestParam(value ="pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy
     ){
-        return postService.getAllPosts(pageNo, pageSize);
+        return postService.getAllPosts(pageNo, pageSize, sortBy);
     }
 
 
@@ -58,3 +59,6 @@ public class PostController {
         return  new ResponseEntity<>("Post entity deleted successfully", HttpStatus.OK);
     }
 }
+
+
+
